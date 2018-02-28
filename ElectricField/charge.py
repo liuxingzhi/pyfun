1bb964732bf51d66c21cdf359331b282f3d9d7c4
xingzhi <xingzhi2@illinois.edu>
2018-02-27 20:41:33 -0600

    make it more OOP

--- ElectricField/charge.py
+++ ElectricField/charge.py
@@ -1,69 +1,73 @@
-# from __future__ import division
 from vpython import *
 
 # scene = display(width=400, height=400)
-scene = canvas(title='show electric field', width=600, height=500, background=color.cyan)
-# constants
+scene = canvas(title='electric field of a list of charges', width=800, height=600, background=color.magenta)
+
+# some useful constants
 k = 9e9  # force constant
 q = 1.6e-19  # unit of charge
-ASCALE = .1
-x = 0
-y = 0
-z = 0
 
-# variables
-l = 5
-L = 10
-N = 5
-Q = 1e-8
-dq = Q / N
-dx = L / N
-dis = 1e-8
 
-# observasion point
-ro = vector(0, L / 4, 0)
+# Electric charge class
+class ElectricBall(sphere):
+    def __init__(self, color=color.red, radius=0, charge=0, pos=vector(0, 0, 0)):
+        self.charge = charge
+        sphere.__init__(self, pos=pos, radius=radius, color=color)
 
-# calc the field
 
-# creat a list of charge
-charges = []
-for x in arange(-5, 5.1, L / N):
-    q = sphere(pos=vector(x, 0, 0), radius=1, color=color.red, charge=dq)
-    charges.append(q)
+# create a list of charges to simulate a eletric bar
+def draw_field(num_charge):
+    # variables
+    # bar length
+    l = num_charge
 
-# arrows = []
-# creat arrow along x axis
-for x in arange(-1 * l, 1.1 * l, 0.3 * l):
-    rate(20)
-    E = 0
-    for y in arange(-1 * l, 1.1 * l, 0.5 * l):
-        rate(20)
-        # arrow1.pos.y = y
-        for z in arange(-1 * l, 1.1 * l, 0.5 * l):
-            rate(20)
-            # arrow1.pos.z = z
-            arrow1 = arrow(pos=vector(x, y, z), color=color.white, opacity=0.0)
-            field = vector(0, 0, 0)
-            # infinity large arrows will be ignored
-            infinity = False
-            # each arrow is affected by all of the charges
-            for q in charges:
-                rate(20)
-                R = arrow1.pos - q.pos
-                # print("what happen?", arrow1.pos, q.pos, R)
-                if mag(R) == 0:
-                    infinity = True
-                    break
-                E = (k * dq / (R.mag) ** 2) * R.norm()
-                # print("E is ", (k * dq / (R.mag) ** 2))
-                # sum electric field at the each point
-                field += E
-            if infinity:
-                continue
+    # total charge, a defined constant
+    Q = 1e-8
+    dq = Q / num_charge
+    # dx = L / num_charge
+    charges = []
+    for x in arange(-l, 1.1 * l, 2 * l / (num_charge - 1)):
+        q = ElectricBall(pos=vector(x, 0, 0), radius=1, color=color.red, charge=dq)
+        charges.append(q)
 
-            #if arrow is not too large, display it
-            if mag(field) < 10:
-                arrow1.axis = field
-                arrow1.shaftwidth = 0.2
-                arrow1.opacity = 1.0
-                # arrows.append(arrow1)
+    # creat arrow around electric balls
+    arrows = []
+    for x in arange(-1 * l, 1.1 * l, 0.3 * l):
+        E = 0
+        for y in arange(-1 * l, 1.1 * l, 0.4 * l):
+            for z in arange(-1 * l, 1.1 * l, 0.4 * l):
+                pointer = arrow(pos=vector(x, y, z), color=color.white, opacity=0.0)
+                total_field = vector(0, 0, 0)
+
+                # infinity large arrows will be ignored
+                infinity = False
+
+                # each arrow is affected by all of the charges
+                for q in charges:
+                    dir = pointer.pos - q.pos
+                    if dir.mag == 0:
+                        infinity = True
+                        break
+                    # calculate electric field affected by each electric ball
+                    E = (k * dq / (dir.mag) ** 2) * dir.norm()
+                    # sum electric field at the each point
+                    total_field += E
+
+                if infinity:
+                    continue
+
+                # if arrow is not too large, display it
+                if total_field.mag < 10:
+                    rate(25)
+                    pointer.axis = total_field
+                    pointer.shaftwidth = 0.2
+                    pointer.opacity = 1.0
+                    arrows.append(pointer)
+
+
+def main():
+    draw_field(9)
+
+
+if __name__ == '__main__':
+    main()
