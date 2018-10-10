7de4f454a7a6260febf217c608bf7d66814afe1e
xingzhi <xingzhi2@illinois.edu>
2018-10-10 02:24:29 -0500

    add imgs function

--- pdfcombiner/pdfUtils.py
+++ pdfcombiner/pdfUtils.py
@@ -1,10 +1,9 @@
-import os, sys, stat
+import os, stat
 import codecs
 import os.path
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 import time
-# import img2pdf
-from PIL import Image
+import img2pdf
 
 
 def timeit(func):
@@ -21,6 +20,7 @@ def timeit(func):
 
 
 def get_pdf_names(path):
+    all_pictures2pdf(path, fixed_size=True)
     """walk can recursively list sub directories"""
     for root, dirs, files in os.walk(path):
         files.sort()
@@ -30,27 +30,31 @@ def get_pdf_names(path):
 
 
 def get_picture_names(path):
+    img_extensions = [".jpg", ".png"]
     for root, dirs, files in os.walk(path):
         for file_name in files:
-            if os.path.splitext(file_name)[1].lower() == ".jpg":
+            if os.path.splitext(file_name)[1].lower() in img_extensions:
                 yield os.path.join(root, file_name)
 
 
-def pictures_to_pdf(path):
+def all_pictures2pdf(path, fixed_size=False):
+    # specify paper size (A4)
+    a4inpt = (img2pdf.mm_to_pt(210), img2pdf.mm_to_pt(297))
+    layout_fun = img2pdf.get_layout_fun(a4inpt) if fixed_size else img2pdf.get_layout_fun()
     for pic_name in get_picture_names(path):
-        print(pic_name)
-        # with open("name.pic_name","wb") as f:
-        # f.write(img2pdf.convert('test.jpg'))
+        pdf_name = pic_name[:-4] + ".pdf"
+        with open(pdf_name, "wb") as pdf:
+            pdf.write(img2pdf.convert(pic_name, layout_fun=layout_fun))
 
 
 @timeit
-def merge_pdf(path, outfile):
-    if os.path.exists(outfile):
-        os.remove(outfile)
+def merge_pdf(path, output_filename):
+    if os.path.exists(output_filename):
+        os.remove(output_filename)
     os.chmod(path, stat.S_IRWXU)
     output_pdf = PdfFileMerger()
     output_page_num = 0
-    for pdf_name in get_picture_names(path):
+    for pdf_name in get_pdf_names(path):
         print(pdf_name)
         with open(pdf_name, "rb") as pdf:
             content = PdfFileReader(pdf)
@@ -64,13 +68,12 @@ def merge_pdf(path, outfile):
             #
             # for i in range(content.numPages):
             #     output_pdf.addPage(content.getPage(i))
-    with codecs.open(outfile, "wb") as f:
+    with codecs.open(output_filename, "wb") as f:
         output_pdf.write(f)
     print("mission complete")
 
 
-# functions to add: combine images to pdfs
 if __name__ == '__main__':
     # print("\n".join(get_file_name('.')))
     # print(os.listdir('.'))
-    merge_pdf("cs241", "combine.pdf")
+    merge_pdf("imgs", "combine.pdf")
