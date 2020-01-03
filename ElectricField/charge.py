e6d9e5abd3af6d0f77a59a7219431bdf66e2cc21
xingzhi <xingzhi2@illinois.edu>
2020-01-03 23:16:08 +0800

    change observe view

--- ElectricField/charge.py
+++ ElectricField/charge.py
@@ -3,6 +3,7 @@ from vpython import *
 # some useful constants
 k = 9e9  # force constant
 q = 1.6e-19  # unit of charge
+view_space_length = 10
 
 
 # Electric charge class
@@ -13,57 +14,56 @@ class ElectricBall(sphere):
 
 
 # create a list of charges to simulate a eletric bar
-def draw_field(num_charge):
+def draw_electrical_field(num_charge):
     scene = canvas(title='electric field of a list of charges', width=800, height=600, background=color.magenta)
-    # variables
-    # bar length
-    l = num_charge
-
-    # total charge, a defined constant
-    Q = 1e-8
-    dq = Q / num_charge
     # dx = L / num_charge
+    Q = 1e-8  # total charges
+    # dq = 1e-8 / 6  # define charge of electrons
+    dq = Q / num_charge
     charges = []
-    for x in arange(-l, 1.1 * l, 2 * l / (num_charge - 1)):
+    space_between = 2 * view_space_length / (num_charge + 1) # evenly divide space between each electrons
+    for x in arange(-view_space_length + space_between, view_space_length, space_between):
         q = ElectricBall(pos=vector(x, 0, 0), radius=1, color=color.red, charge=dq)
         charges.append(q)
 
     # creat arrow around electric balls
     arrows = []
-    for x in arange(-1.5 * l, 1.51 * l, 0.3 * l):
-        for y in arange(-1 * l, 1.1 * l, 0.4 * l):
-            for z in arange(-1 * l, 1.1 * l, 0.4 * l):
-                pointer = arrow(pos=vector(x, y, z), color=color.white, opacity=0.0)
-                total_field = vector(0, 0, 0)
+    observe_points_dis = 0.5 * view_space_length
+    for x in arange(-1.5 * view_space_length, 1.51 * view_space_length, observe_points_dis):
+        for y in arange(-1.0 * view_space_length, 1.01 * view_space_length, observe_points_dis):
+            for z in arange(-1.0 * view_space_length, 1.01 * view_space_length, observe_points_dis):
+                pointer = arrow(pos=vector(x, y, z), color=color.blue, opacity=0.0)
+                electrical_vector = vector(0, 0, 0)
 
                 # infinity large arrows will be ignored
                 infinity = False
 
                 # each arrow is affected by all of the charges
                 for q in charges:
-                    dir = pointer.pos - q.pos
-                    if dir.mag == 0:
+                    direction_vector = pointer.pos - q.pos
+                    if direction_vector.mag == 0:
                         infinity = True
                         break
                     # calculate electric field affected by each electric ball
-                    E = (k * dq / (dir.mag) ** 2) * dir.norm()
+                    E = (k * dq / (direction_vector.mag) ** 2) * direction_vector.norm()
                     # sum electric field at the each point
-                    total_field += E
+                    electrical_vector += E
 
                 if infinity:
                     continue
 
                 # if arrow is not too large, display it
-                if total_field.mag < 10:
-                    rate(25)
-                    pointer.axis = total_field
-                    pointer.shaftwidth = 0.2
+                if electrical_vector.mag < observe_points_dis:
+                    # "rate(30)" tells the program to not do the loop more than 30 times a second.
+                    rate(20 * num_charge)
+                    pointer.axis = electrical_vector
+                    pointer.shaftwidth = 0.1
                     pointer.opacity = 1.0
                     arrows.append(pointer)
 
 
 def main():
-    draw_field(8)
+    draw_electrical_field(200)
 
 
 if __name__ == '__main__':
