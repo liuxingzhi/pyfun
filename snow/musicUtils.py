dcbe501732db44b2c2ded9005445651ddd0b6bcc
xingzhi <xingzhi2@illinois.edu>
2020-01-12 11:41:47 +0800

    add background music

--- snow/musicUtils.py
+++ snow/musicUtils.py
@@ -5,9 +5,10 @@ from time import sleep
 
 
 class LoopMusic(Thread):
-    def __init__(self, music, loop=1, forever=False):
+    def __init__(self, music, loop=1, interval=3, forever=False):
         super(LoopMusic, self).__init__()
         self.audio = AudioSegment.from_mp3(music)
+        self.interval = interval
         if forever:
             self.loop = float("inf")
         else:
@@ -17,12 +18,12 @@ class LoopMusic(Thread):
         while self.loop >= 1:
             play(self.audio)
             self.loop -= 1
-            sleep(5)
+            sleep(self.interval)
 
 
 class BackgroundMusic:
-    def __init__(self, song_name: str, forever=True):
-        self.thread = LoopMusic(song_name, forever=forever)
+    def __init__(self, song_name: str, interval=3, forever=True):
+        self.thread = LoopMusic(song_name, interval=interval, forever=forever)
         self.thread.setDaemon(True)
 
     def run(self):
