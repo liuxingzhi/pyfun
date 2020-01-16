0f3d8dca0401ad30ced0d9db6e88a9185dac2285
xingzhi <xingzhi2@illinois.edu>
2020-01-16 23:36:01 +0800

    change AudioSegment to Background music class

--- snow/musicUtils.py
+++ snow/musicUtils.py
@@ -7,7 +7,7 @@ from time import sleep
 class LoopMusic(Thread):
     def __init__(self, music, loop=1, interval=3, forever=False):
         super(LoopMusic, self).__init__()
-        self.audio = AudioSegment.from_file(music)
+        self.audio = music
         self.interval = interval
         if forever:
             self.loop = float("inf")
@@ -23,7 +23,9 @@ class LoopMusic(Thread):
 
 class BackgroundMusic:
     def __init__(self, song_name: str, loop=1, interval=3, forever=True):
-        self.thread = LoopMusic(song_name, loop=loop, interval=interval, forever=forever)
+        self.song_name = song_name
+        self.music = AudioSegment.from_file(song_name)
+        self.thread = LoopMusic(self.music, loop=loop, interval=interval, forever=forever)
         self.thread.setDaemon(True)
 
     def run(self):
